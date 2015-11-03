b1a87434272a99a82a228df688ad3ea96ac104f0
xolf UG iG <xolf@users.noreply.github.com>
2015-11-03 20:41:28 +0100

    Added require function
    You can now require .js or .css files with the Layout class.

--- Layout.php
+++ Layout.php
@@ -18,6 +18,17 @@ class Layout
         $this->view->inc('layouts.' . $this->layout . 'head', $this->data);
 
     }
+    
+    public function require($file)
+    {
+        
+        $file = explode('.',$file);
+        
+        if($file[1] == 'css') echo '<link href="'.$file[0].'" rel="stylesheet">';
+        
+        if($file[1] == 'js') echo '<script src="'.$file[0]'"></script>';
+        
+    }
 
     public function __destruct()
     {
