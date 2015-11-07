d76f04d8f88f78ece7dc35ab25e50ee6bbc542c0
f.theimer <paypal.xolf@yahoo.com>
2015-11-07 23:21:11 +0100

    I

--- Translate.php
+++ Translate.php
@@ -11,7 +11,7 @@ class Translate
 
         $this->lang = $lang;
 
-        define('DREAMCOIL_LANG', $lang);
+        define('DREAMCOILLANG', $lang);
 
     }
 
@@ -24,7 +24,7 @@ class Translate
 
         $fallback = $config->get('fallback_lang');
 
-        if('DREAMCOIL_LANG' === null)
+        if(DREAMCOILLANG === null)
         {
             if(!file_exists( __DIR__ . '/../files/Translations/' . $fallback . '/'. $key[0] . '.php'))
                 return implode('.', $key);
@@ -37,10 +37,10 @@ class Translate
 
         }
 
-        if(!file_exists( __DIR__ . '/../files/Translations/' . 'DREAMCOIL_LANG' . '/'. $key[0] . '.php'))
+        if(!file_exists( __DIR__ . '/../files/Translations/' . DREAMCOILLANG . '/'. $key[0] . '.php'))
             return implode('.', $key);
 
-        $lang = include __DIR__ . '/../files/Translations/' . 'DREAMCOIL_LANG' . '/'. $key[0] . '.php';
+        $lang = include __DIR__ . '/../files/Translations/' . DREAMCOILLANG . '/'. $key[0] . '.php';
 
         if(isset($lang[$key[1]])) return $lang[$key[1]];
 
