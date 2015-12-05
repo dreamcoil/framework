ddfdbb0c2ef5c1598fa1fae7e06e0c36e97b6580
xolf <webmaster@xolf.info>
2015-12-05 14:00:17 +0100

    Fixed issue: #6 :zap:

--- Dreamcoil.php
+++ Dreamcoil.php
@@ -6,7 +6,9 @@ class Dreamcoil
 	public function url($url)
 	{
 
-		$route = str_replace(ROUTE, '', $_SERVER['REQUEST_URI']);
+		$route = $_SERVER['REQUEST_URI'];
+
+		if(ROUTE != '/' ) $route = str_replace(ROUTE, '', $_SERVER['REQUEST_URI']);
 
 		return $route . $url;
 
