8b5167256ffb1ec746d41cf516708771fdbe2674
xolf <webmaster@xolf.info>
2015-12-12 21:41:28 +0100

    Verify can now vailidate passwords :yellow_heart:

--- Codebowl/Verify.php
+++ Codebowl/Verify.php
@@ -12,4 +12,38 @@ class Verify
 
 	}
 
+	public function password($password, $length = 6, $strength = 0)
+	{
+
+		if(strlen($password) >= $length)
+		{
+
+			switch ($strength)
+			{
+
+				case 0:
+					return true;
+					break;
+
+				case 1:	
+					if(preg_match("([a-z])", $password) && preg_match("([A-Z])", $password)) return true;
+					return false;
+					break;
+
+				case 2:
+					if(preg_match("([a-z])", $password) && preg_match("([A-Z])", $password) && preg_match("([1-9])", $password)) return true;
+					return false;
+					break;
+
+				case 3:
+					if(preg_match("(?=(.*[a-z])+)(?=(.*[0-9])+)[0-9a-zA-Z!\"#$%&'()*+,\-.\/:;<=>?@\[\\\]^_`{|}~]{" . $length . ",99}", $password)) return true;
+					return false;
+					break;
+			}
+
+		}
+		else return false;
+
+	}
+
 }
