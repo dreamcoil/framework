d2b359b841fbedd34af08aaf92fee4a38b5a093c
xolf <webmaster@xolf.info>
2015-12-14 21:11:24 +0100

    Improved Dreamcoil::url()
    Dreamcoil url() can now return full urls to given objects!

--- Dreamcoil.php
+++ Dreamcoil.php
@@ -3,12 +3,14 @@
 class Dreamcoil 
 {
 
-	public function url($url)
+	public function url($url, $full = false)
 	{
 
 		$route = $_SERVER['REQUEST_URI'];
 
 		if(ROUTE != '/' ) $route = str_replace(ROUTE, '', $_SERVER['REQUEST_URI']);
+
+		if($full) return 'http://'  . $_SERVER['SERVER_NAME'] . $route . $url;
 
 		return $route . $url;
 
