9d285ae564bda9b64dd7b3dc7a1c08870c170a17
xolf <webmaster@xolf.info>
2015-12-21 12:46:58 +0100

    :rage2: Removed the cool new features!

--- Phase.php
+++ Phase.php
@@ -5,7 +5,6 @@ namespace Dreamcoil;
 class Phase 
 {
 	public $config, $translate, $auth;
-	private $error;
 
 	public function __construct()
 	{
@@ -15,32 +14,6 @@ class Phase
 		$this->translate = new \Dreamcoil\Translate;
 
         $this->auth = new \Dreamcoil\Auth();
-
-	}
-
-    /**
-     * Returns an error
-     *
-     * @param $id
-     * @return string
-     */
-	public function getError()
-	{
-
-		return $this->error;
-
-	}
-
-    /**
-     * Sets an error
-     *
-     * @param string $error
-     * @return string
-     */
-	public function setError(string $error)
-	{
-
-		$this->error = $error;
 
 	}
 
