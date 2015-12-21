521e51487ebdf4cf4b4443e32aaab6427b13042f
xolf <webmaster@xolf.info>
2015-12-21 12:36:08 +0100

    :triangular_flag_on_post: Phase gots an error system

--- Phase.php
+++ Phase.php
@@ -5,6 +5,7 @@ namespace Dreamcoil;
 class Phase 
 {
 	public $config, $translate, $auth;
+	private $error;
 
 	public function __construct()
 	{
@@ -14,6 +15,32 @@ class Phase
 		$this->translate = new \Dreamcoil\Translate;
 
         $this->auth = new \Dreamcoil\Auth();
+
+	}
+
+    /**
+     * Returns an error
+     *
+     * @param $id
+     * @return string
+     */
+	public function getError()
+	{
+
+		return $this->error;
+
+	}
+
+    /**
+     * Sets an error
+     *
+     * @param string $error
+     * @return string
+     */
+	public function setError(string $error)
+	{
+
+		$this->error = $error;
 
 	}
 
