7b4151a8896c2a89a9361c902582acc133ab0bdd
xolf <xolf@users.noreply.github.com>
2016-02-02 20:04:54 +0100

    Update Console.php

--- Console.php
+++ Console.php
@@ -5,10 +5,17 @@ namespace Dreamcoil;
 class Console
 {
 
+    public function format($text, $color)
+    {
+        
+        return chr(27) . "$color" .  "$text" . chr(27) . "[0m";
+        
+    }
+
     public function write($text, $color)
     {
         
-        echo chr(27) . "$color" .  "$text" . chr(27) . "[0m";
+        echo "\n" . $this->format($text, $color);
         
     }
 
