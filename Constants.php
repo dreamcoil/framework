7d195f94bb393e57b373e60678d6e6d28b42ee46
Lukas Mueller <dashomeradio@outlook.de>
2016-02-02 19:48:29 +0100

    Added new Constants for console

--- Constants.php
+++ Constants.php
@@ -20,9 +20,46 @@ define('ONE_WEEK', 604800);
 
 define('ONE_YEAR', 30585600);
 
+//Web log
 
 define('LOG_INFO_TEXT', '[info]');
 
 define('LOG_WARN_TEXT', '[Warn]');
 
 define('LOG_ERROR_TEXT', '[ERROR]');
+
+//Console text color
+
+define('CONSOLE_TEXT_BLACK', "[30m");
+
+define('CONSOLE_TEXT_RED', "[31m");
+
+define('CONSOLE_TEXT_GREEN', "[32m");
+
+define('CONSOLE_TEXT_YELLOW', "[33m");
+
+define('CONSOLE_TEXT_BLUE', "[34m");
+
+define('CONSOLE_TEXT_MAGENTA', "[35m");
+
+define('CONSOLE_TEXT_CYAN', "[36m");
+
+define('CONSOLE_TEXT_WHITE', "[37m");
+
+//Console background color
+
+define('CONSOLE_BACKGROUND_BLACK', "[40m");
+
+define('CONSOLE_BACKGROUND_RED', "[41m");
+
+define('CONSOLE_BACKGROUND_GREEN', "[42m");
+
+define('CONSOLE_BACKGROUND_YELLOW', "[43m");
+
+define('CONSOLE_BACKGROUND_BLUE', "[44m");
+
+define('CONSOLE_BACKGROUND_MAGENTA', "[45m");
+
+define('CONSOLE_BACKGROUND_CYAN', "[46m");
+
+define('CONSOLE_BACKGROUND_WHITE', "[47m");
