a36ee475774c2c48421effe6ca20251635d101f5
xolf <xolf@users.noreply.github.com>
2016-04-06 14:30:20 +0100

    FIX

--- Dreamcoil.php
+++ Dreamcoil.php
@@ -111,7 +111,7 @@ class Dreamcoil
 	 */
 	public function isBot()
 	{
-		return isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/bot|crawl|slurp|spider/i', $_SERVER['HTTP_USER_AGENT'])
+		return isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/bot|crawl|slurp|spider/i', $_SERVER['HTTP_USER_AGENT']);
 	}
     
 }
