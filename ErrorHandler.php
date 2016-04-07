8119e1f93036776eac031a2f40b765032c27b462
xolf <xolf@users.noreply.github.com>
2016-04-07 18:11:05 +0100

    Delete ErrorHandler.php

--- ErrorHandler.php
+++
@@ -1,9 +0,0 @@
-<?php 
-     function DreamcoilErrorHandler($errno, $errstr,$error_file,$error_line) {
-        echo "<b>Error:</b> [$errno] $errstr - $error_file:$error_line";
-        echo "<br />";
-        echo "Terminating Dreamcoil Framework";
-          
-        die();
-   }
-?>
