31fb0fb2a24bc1a13da887b92c89cb4d8fff174d
xolf <xolf@users.noreply.github.com>
2016-04-07 18:05:03 +0100

    Update ErrorHander.php

--- ErrorHander.php
+++ ErrorHander.php
@@ -1,9 +1,9 @@
 <?php 
-function handleError($errno, $errstr,$error_file,$error_line) {
-      echo "<b>Error:</b> [$errno] $errstr - $error_file:$error_line";
-      echo "<br />";
-      echo "Terminating PHP Script";
-      
-      die();
+     function DreamcoilErrorHandler($errno, $errstr,$error_file,$error_line) {
+        echo "<b>Error:</b> [$errno] $errstr - $error_file:$error_line";
+        echo "<br />";
+        echo "Terminating Dreamcoil Framework";
+          
+        die();
    }
 ?>
