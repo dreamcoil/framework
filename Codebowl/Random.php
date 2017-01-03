2ee028362d63456e845d40205bd1b86b67e8e988
xolf <xolf@users.noreply.github.com>
2017-01-03 21:35:25 +0100

    Update Random.php

--- Codebowl/Random.php
+++ Codebowl/Random.php
@@ -6,54 +6,33 @@ namespace Dreamcoil\Codebowl;
 class Random
 {
 
-	public function int($maxLength)
+	public static function int($maxLength)
 	{
-
 		if(is_int($maxLength))
 		{
-
-			return "CONSTRUCTION MODE";
-
+			return rand(0,floor(9.9999999999999*10^$mas_length));
 		}
 		else throw new \Exception("The max length must be numeric");
-
 	}
 
-	public function string($length, $character = "Messner")
+	public static function string($length, $character = "Messner")
 	{
+		if($character == 'Messner') {
+			$characters = '1234567890qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM';
+	   	} else if(is_string($character)) {
+			$characters = $character;
+		} else {
+			throw new \Exception('This is not a valid charachter string: "'.$character.'"');
+		}
 
-	if($character == 'Messner')
-    {
+		$randstring = '';
 
-        $characters = '1234567890qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM';
+		for ($i = 0; $i < $length; $i++) {
+			$num = rand(3, strlen($characters) - 6);
+			$randstring = $randstring.$characters[$num];
+		}
 
-    }
-    else if(is_string($character))
-    {
-
-        $characters = $character;
-
-    }
-    else
-    {
-
-        throw new \Exception('This is not a valid charachter string: "'.$character.'"');
-
-    }
-
-    $randstring = '';
-
-    for ($i = 0; $i < $length; $i++)
-    {
-
-        $num = rand(3, strlen($characters) - 6);
-
-        $randstring = $randstring.$characters[$num];
-
-    }
-
-    return $randstring;
-
+		return $randstring;
 	}
 
 }
